db2262594c7b4d44bae5020872c2a7c9b0b90dea
Nicholas Lee <nlee05@uafs.edu>
2026-09-03 11:47:47 -0500

    Updated - Displaying Array

--- assign1/assign1/Driver.java
+++ assign1/assign1/Driver.java
@@ -3,7 +3,15 @@ package assign1;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		int searchOrder = 1005;
+		int getOrder = 4; 
+		OrderDBInterface order = new Order();
+		order.loadOrders("orders.txt");
+		order.showOrders();
+		System.out.printf("\n - Total number of orders are \t\t--- %d\n",order.size());
+		System.out.printf(" - Allocated array size: \t\t--- %d elements\n", order.capacity());
+		System.out.printf(" - Order # %d is \t\t\t--- %s\n",getOrder, order.get(getOrder));
+		System.out.printf(" - Order # %d is stored in element: \t--- %d of the array\n", searchOrder, order.searchByOrderID(searchOrder));
 	}
 
 }
--- assign1/assign1/Order.java
+++ assign1/assign1/Order.java
@@ -1,10 +1,52 @@
 package assign1;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 
-public class Order implements OrderDBInterface	{
+
+public class Order implements OrderDBInterface 	{
+	int orderCount;
+	int orderNum;
+	int orderID;
+	public String fileName;
+	public Order getOrder;
+	double totalAmt;
+	String product;
+	String orderDate;
+	String customerName;
+	
+	Order[] order = new Order[100];
+
+	public Order(String record) {
+		String[] fields = record.split(",");
+		this.setOrderID(Integer.parseInt(fields[0]));
+		this.setCustomerName(fields[1]);
+		this.setProduct(fields[2]);
+		this.setTotalAmount(Double.parseDouble(fields[3]));
+		this.setOrderDate(fields[4]);
+	}
 
 	@Override
 	public int loadOrders(String fileName) {
-		return 0;
+		String record;
+		Scanner orderFile;
+		
+		try {
+			orderFile = new Scanner(new File(fileName));
+			record = orderFile.nextLine();  //Read File Header
+			
+			while(orderFile.hasNext()) {		
+				record = orderFile.nextLine();  
+				order[orderCount] = new Order(record);
+				orderCount++;
+				
+			}
+			orderFile.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return 0;	
 	}
 
 	@Override
@@ -14,7 +56,14 @@ public class Order implements OrderDBInterface	{
 
 	@Override
 	public void showOrders() {
+		int index = 0;
 		
+		System.out.printf("%-8s %-30s %10s\n","Order ID","Product", "Total Amt"); 
+		System.out.printf("%-8s %-30s %10s\n","--------","-------", "---------"); 
+		while(order[index] != null) {
+			System.out.printf("%-+8d %-30s %10.2f\n", order[index].getOrderID(), order[index].getProduct(), order[index].getTotalAmt());
+			index++;
+		}
 	}
 
 	@Override
@@ -34,12 +83,21 @@ public class Order implements OrderDBInterface	{
 
 	@Override
 	public Order get(int index) {
-		return null;
+		return this.order[index-1];
 	}
 
 	@Override
 	public int searchByOrderID(int orderID) {
-		return 0;
+		int orderID1 = 0;
+		
+		for(int index = 0; index < order.length; index++) {
+			orderID1 = order[index].getOrderID();
+			if(orderID1 == orderID) {
+				return index+1;
+			}
+		}
+		
+		return -1;
 	}
 
 	@Override
@@ -54,17 +112,57 @@ public class Order implements OrderDBInterface	{
 
 	@Override
 	public int size() {
-		return 0;
+		return orderCount;
 	}
 
 	@Override
 	public int capacity() {
-		return 0;
+		return order.length;
 	}
 
 	@Override
 	public void resize() {
 		
+	}
+	
+	public Order() {
+		
+	}
+
+	
+	
+	private void setOrderDate(String string) {  //Getters and Setters to display orders. 
+		this.orderDate = string;
+	}
+	private void setTotalAmount(double double1) {
+		this.totalAmt = double1;
+	}
+	private void setProduct(String string) {
+		this.product = string; 
+	}
+	private void setCustomerName(String string) {
+		this.customerName = string; 
+	}
+	private void setOrderID(int int1) {
+		this.orderID = int1;
+	}
+	private String getOrderDate() {
+		return this.orderDate;
+	}
+	private double getTotalAmt() {
+		return this.totalAmt;
+	}
+	private String getProduct() {
+		return this.product;
+	}
+	private String getCustomerName() {
+		return customerName;
+	}
+	private int getOrderID() {
+		return orderID;
+	}
+	public String toString() {
+		return "Order ID: " + orderID + ", Product: " + product + ", Total Amount: " + totalAmt;
 	}
 
 }
